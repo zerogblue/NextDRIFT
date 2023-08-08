b4966f5aa0199d5b95c6f2fcce58ce65843eeffe
Jahanzaib <98064270+zerogblue@users.noreply.github.com>
2023-08-07 23:45:08 -0400

    Test

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+var path = require('path');
+const routes = require('./routes/routes');
+
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
+console.log(mongoString);
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+//
+
+const app = express();
+app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/main.html'))
+})
+
+app.get('/create-events', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/create-event.html'))
+})
+
+app.get('/events', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/events.html'))
+})
+
+app.use(express.json());
+app.use('/api', routes);
+
+
+app.listen(3000, () => {
+    console.log('Server launched at 3000')
+})
---
+++ models/model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    location: {
+        required: true,
+        type: String
+    }
+})
+
+module.exports = mongoose.model('data', dataSchema)
